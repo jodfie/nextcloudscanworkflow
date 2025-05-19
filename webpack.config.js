cb4f94d3e1991e9480cde2246fecec7d55f78072
Jody <jodfie@gmail.com>
2025-05-19 04:10:18 +0000

    update packaging

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue: '@vue/runtime-dom'
+    },
+    extensions: ['.js', '.vue']
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
