b1c2aeeb74017483ba7ccf63b8410c3749d59477
Jody <jodfie@gmail.com>
2025-05-19 01:21:33 +0000

    updates

---
+++ app.php
@@ -0,0 +1,7 @@
+<?php
+script('nextcloudscanworkflow', 'main'); // loads Vue JS
+style('nextcloudscanworkflow', 'style'); // optional CSS
+
+?>
+
+<div id="app"></div>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jodfie/pdfworkflow',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4a1dda81b4e2dcaee654be7b0d6ac1954f5c1e2c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jodfie/pdfworkflow' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4a1dda81b4e2dcaee654be7b0d6ac1954f5c1e2c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
